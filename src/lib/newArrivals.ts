f85859ca01c2589f03a89c9eb9bfc1dc5ecc9eb9
Saad <saadtariq57000@gmail.com>
2025-03-11 20:56:29 +0500

    Debugging New Arrivals

--- src/lib/newArrivals.ts
+++ src/lib/newArrivals.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export async function getNewArrivals() {
-    const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/user/product/get/newArrivals`);
+    const response = await axios.get(`/api/user/product/get/newArrivals`);
     const data = await response.data();
     return data.products; // Assuming the data structure has products
   }
